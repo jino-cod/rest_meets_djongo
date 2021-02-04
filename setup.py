bec3ee0843222d6c62b189673d07e426c789af01
Jino Raymundo <71753933+jino-cod@users.noreply.github.com>
2021-02-04 19:09:51 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         'Django',
         'djangorestframework<4',
-        'djongo>=1.3.0,<1.3.1',
+        'djongo>=1.3.0',
     ],
 
     setup_requires=['pytest-runner'],
